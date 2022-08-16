236e19a9bd89437e71ff12c5006605d8e15918a3
Alfie Mansfield <asm195@uclive.ac.nz>
2022-08-17 11:34:34 +1200

    update

--- assignment1/partA/fitCurve.py
+++ assignment1/partA/fitCurve.py
@@ -1,10 +1,12 @@
 from numpy import loadtxt
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import subplots, show
 from scipy.optimize import curve_fit
 
-def model(x,a,b,c):
-    return a * np.exp(-b *x) + c 
+def model(x,a,b,c,d):
+    return 0.5*a * np.exp(-0.5*b *x) + c*x +d 
+
 # Test
 # Load data
 filename = 'assignment1/partA/calibration.csv'
@@ -13,11 +15,63 @@ IR3Data = loadtxt(filename, delimiter=',',skiprows=1, usecols=(2,6))
 x, y = IR3Data.T
 
 params, cov = curve_fit(model,x, y)
-yfit = model(x,*params)
-yerror = y- yfit
+IRfit = model(x,*params)
+IRerror = y - IRfit
+
+fig, axes = subplots(3)
+fig.suptitle('Calibration IR3 ')
+
+axes[0].plot(x, y, '.')
+axes[0].plot(x, IRfit, '.' )
+axes[0].set_title('IR3 raw data')
 
 
-plt.plot(x,y)
-plt.plot(x,yfit)
-plt.plot(x,yerror)
-plt.show()
+axes[1].plot(x, IRerror, '.')
+axes[1].set_title('measurement error')
+
+axes[2].hist(IRerror, bins=100)
+axes[2].set_title('histogram')
+
+
+upperLim = 0.1
+lowerLim = -0.1
+fittedx = []
+fittedy = []
+print(len(x))
+print(len(y))
+for i in range(len(IRerror)):
+    if (IRerror[i] < upperLim) & (IRerror[i] > lowerLim):
+        fittedx.append(x[i])
+        fittedy.append(y[i])
+    else:
+        pass
+
+fittedx = np.array(fittedx)
+fittedy = np.array(fittedy)
+        
+
+params, cov = curve_fit(model,fittedx, fittedy)
+fittedIRfit = model(fittedx,*params)
+fittedIRerror = fittedy - fittedIRfit
+
+
+
+fig, axes = subplots(3)
+fig.suptitle('Refitted IR3 plots ')
+
+axes[0].plot(fittedx, fittedy, '.')
+axes[0].plot(fittedx, fittedIRfit, '.' )
+axes[0].set_title('IR3 raw data')
+
+
+axes[1].plot(fittedx, fittedIRerror, '.')
+axes[1].set_title('measurement error')
+
+axes[2].hist(fittedIRerror, bins=100)
+axes[2].set_title('histogram')
+
+
+
+show()
+
+
