d9c0837fe57993e99ae1064fc91ba5c4815021fb
bsh75 <100744471+bsh75@users.noreply.github.com>
2022-08-16 13:31:11 +1200

    Add files via upload

---
+++ assignment1/partA/plot-speeds.py
@@ -0,0 +1,24 @@
+from numpy import loadtxt, gradient
+from matplotlib.pyplot import subplots, show
+
+# Load data
+filename = 'training1.csv'
+data = loadtxt(filename, delimiter=',', skiprows=1)
+
+# Split into columns
+index, time, distance, velocity_command, raw_ir1, raw_ir2, raw_ir3, raw_ir4, \
+    sonar1, sonar2 = data.T
+
+v_com = velocity_command
+
+dt = time[1:] - time[0:-1]
+v_est = gradient(distance, time)
+
+fig, axes = subplots(1)
+axes.plot(time, v_com, label='commanded speed')
+axes.plot(time, v_est, label='measured speed')
+axes.set_xlabel('Time')
+axes.set_ylabel('Speed (m/s)')
+axes.legend()
+
+show()
