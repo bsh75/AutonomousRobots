ff2c1d0b369f25d6630d8feecf4b6f687c1e4fe6
Brett Hockey <bsh75@uclive.ac.nz>
2022-08-17 14:46:59 +1200

    Model for Sonar looking good

--- assignment1/partA/fitCurveSonar.py
+++ assignment1/partA/fitCurveSonar.py
@@ -5,82 +5,75 @@ from matplotlib.pyplot import subplots, show
 from scipy.optimize import curve_fit
 
 def model(x,a,b):
-    return a * x + b 
+    return a * x + b
+
 # Test
 # Load data
 filename = 'assignment1/partA/calibration.csv'
 S1Data = loadtxt(filename, delimiter=',',skiprows=1, usecols=(2,8))
-
 distance, rawS1 = S1Data.T
 
 params, cov = curve_fit(model, distance, rawS1)
 fit = model(distance,*params)
 error = rawS1 - fit
 
+# Plot original data
 fig, axes = subplots(2)
 fig.suptitle('Calibration Sonar 1')
-
 axes[0].plot(distance, rawS1, '.', alpha=0.2)
 axes[0].plot(distance, fit)
 axes[0].set_title('Plot plus fit')
-
 axes[1].plot(distance, error, '.', alpha=0.2)
 axes[1].set_title('error')
 
+# 1st iteration
+n = 0 # index for outer loop
+N = 4 # How many times the data is cleaned
+errorMin = -0.3 # Eyeballed min value that appears on error plot
+errorMax = 0.7 # Eyeballed max value
 
-distance = distance.tolist()
-error = error.tolist()
-rawS1 = rawS1.tolist()
+while n <= N:
+    n += 1
+    if n == 3:
+        errorMin = -0.1 # Eyeballed min val on second to last error plot
+        errorMax = 0.1 # Eyeballed max val on second to last error plot
+    # Convert back to list
+    distance = distance.tolist()
+    error = error.tolist()
+    rawS1 = rawS1.tolist()
 
-# print(type(distance))
-# print(type(error))
-# print(type(rawS1))
-# print(distance)
-# print(error)
-# print(rawS1)
+    # Remove outliers
+    i = 0
+    while i < len(error):
+        # print(error[i])
+        if error[i] > errorMax:
+            distance.pop(i)
+            rawS1.pop(i)
+            error.pop(i)
+        elif error[i] < errorMin:
+            distance.pop(i)
+            rawS1.pop(i)
+            error.pop(i)
+        i += 1
+    # Convert back to array for plotting
+    distance = np.array(distance)
+    rawS1 = np.array(rawS1)   
 
-# Remove outliers
-print(len(distance))
-print(len(error))
-print(len(rawS1))
+    params, cov = curve_fit(model, distance, rawS1)
+    fit = model(distance,*params)
+    error = rawS1 - fit
 
-errorMin = -0.3 # Min value that appears on error plot
-errorMax = 0.7
-i = 0
-while i < len(error):
-    print(error[i])
-    if error[i] > errorMax:
-        distance.pop(i)
-        rawS1.pop(i)
-        error.pop(i)
-    elif error[i] < errorMin:
-        distance.pop(i)
-        rawS1.pop(i)
-        error.pop(i)
-    i += 1
+fig2, axes = subplots(3)
+fig2.suptitle('Calibration Sonar 1 (Second)')
+axes[0].plot(distance, rawS1, '.', alpha=0.2)
+axes[0].plot(distance, fit)
+axes[0].set_title('Plot plus fit')
+axes[1].plot(distance, error, '.', alpha=0.2)
+axes[1].set_title('error')
+axes[2].hist(error, bins=100)
+axes[2].set_title('histogram')
 
-distance1 = np.array(distance)
-rawS11 = np.array(rawS1)   
+print(params)
+print(np.std(error), np.mean(error))
 
-params, cov = curve_fit(model, distance1, rawS11)
-fit = model(distance1,*params)
-error1 = rawS11 - fit
-
-print(len(distance1))
-print(len(error1))
-print(len(rawS11))
-
-fig2, axes = subplots(2)
-fig2.suptitle('Calibration Sonar 1')
-
-axes[0].plot(distance1, rawS11, '.', alpha=0.2)
-axes[0].plot(distance1, fit)
-axes[0].set_title('Plot plus fit1')
-
-axes[1].plot(distance1, error1, '.', alpha=0.2)
-axes[1].set_title('error1')
-
-# plt.plot(x,y, '.')
-# plt.plot(x,yfit)
-# plt.plot(x,yerror, '.')
 plt.show()
